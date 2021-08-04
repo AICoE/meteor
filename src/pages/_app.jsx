61b1c4859d33e49ea741be945becfb75d7829866
Tomas Coufal <tcoufal@redhat.com>
2021-08-04 19:50:21 +0200

    feat: Enable Google Analytics
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -14,3 +14,12 @@ App.getInitialProps = async (appctx) => {
 };
 
 export default App;
+
+export function reportWebVitals({ id, name, label, value }) {
+  window.gtag('event', name, {
+    event_category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value),
+    event_label: id,
+    non_interaction: true,
+  });
+}
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
+import { GA_TRACKING_ID } from '../constants';
+
 class Document extends NextDocument {
   static async getInitialProps(ctx) {
     const initialProps = await NextDocument.getInitialProps(ctx);
@@ -14,6 +16,19 @@ class Document extends NextDocument {
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/logo192.png" />
           <link rel="manifest" href="/manifest.json" />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
