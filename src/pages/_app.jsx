da7579ee20f9023bf237a046582d8771dade298d
Tom Coufal <7453394+tumido@users.noreply.github.com>
2021-08-05 17:01:50 +0200

    feat: Add prometheus metrics to Meteor Shower (#66)
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,4 +3,14 @@ import App from 'next/app';
 import '@patternfly/react-core/dist/styles/base.css';
 import '../styles.css';
 
+// Populate initial props passed to the React application. Also serves as a middleware intercepting any incoming request.
+App.getInitialProps = async (appctx) => {
+  if (typeof window === 'undefined') {
+    // Only server-side evals should result in prometheus imports. Client side imports doesn't make sense here.
+    const metrics = require('../metrics');
+    metrics.httpRequestsTotal.labels({ method: appctx.ctx.req.method, statusCode: appctx.ctx.res.statusCode, path: appctx.ctx.req.url }).inc();
+  }
+  return {};
+};
+
 export default App;
--- src/pages/api/meteor/[id].ts
+++ src/pages/api/meteor/[id].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getMeteor } from '../../../k8s';
 import { HttpStatusCode } from '../../../constants';
+import { httpRequestsTotal } from '../../../metrics';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
@@ -12,12 +13,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const meteor = await getMeteor(id as string);
     if (meteor) {
       res.status(HttpStatusCode.OK).json(meteor);
+      httpRequestsTotal.labels({ method, statusCode: HttpStatusCode.OK, path: `/api/meteor/${id}` }).inc();
     } else {
       res.status(HttpStatusCode.NOT_FOUND).end('Not Found');
+      httpRequestsTotal.labels({ method, statusCode: HttpStatusCode.NOT_FOUND, path: `/api/meteor/${id}` }).inc();
     }
   } else {
     res.status(HttpStatusCode.METHOD_NOT_ALLOWED).setHeader('Allow', ['GET']);
     res.end(`Method ${method} Not Allowed`);
+    httpRequestsTotal.labels({ method, statusCode: HttpStatusCode.METHOD_NOT_ALLOWED, path: `/api/meteor/${id}` }).inc();
   }
 };
 
