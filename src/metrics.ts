a62ff9f5de5289db1bfa7f500f1ae21eac134bf1
Tomas Coufal <tcoufal@redhat.com>
2021-09-01 22:30:54 +0200

    feat: Prefix and scrape metrics
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- src/metrics.ts
+++ src/metrics.ts
@@ -4,12 +4,12 @@ import { collectDefaultMetrics, Counter, register } from 'prom-client';
 // Hence to support hot reloading we need to flush existing metrics registry in case a page containing a metric was changed.
 if (process.env.NODE_ENV == 'development') {
   register.clear();
-  collectDefaultMetrics();
+  collectDefaultMetrics({ prefix: 'meteor_shower_' });
 }
 
 // Arbitrary metrics counting page hits
 export const httpRequestsTotal = new Counter({
-  name: 'http_requests_total',
+  name: 'meteor_shower_http_requests_total',
   help: 'Total requests received',
   labelNames: ['method', 'statusCode', 'path'],
 });
