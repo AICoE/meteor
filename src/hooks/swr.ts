f1bd0f73412959e6217098179980cedb7f88f05e
Tomas Coufal <tcoufal@redhat.com>
2021-11-29 15:22:28 +0100

    fix: Error interface in typescript
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- src/hooks/swr.ts
+++ src/hooks/swr.ts
@@ -1,10 +1,15 @@
 import useSWR, { mutate } from 'swr';
 import useDebounce from './useDebounce';
 
+interface CustomError extends Error {
+  status: number;
+  info: any;
+}
+
 const fetcher = async (url: string) => {
   const res = await fetch(url);
   if (!res.ok) {
-    const error = new Error('An error occurred while fetching the data.');
+    const error = new Error('An error occurred while fetching the data.') as CustomError;
     error.info = await res.json();
     error.status = res.status;
     throw error;
